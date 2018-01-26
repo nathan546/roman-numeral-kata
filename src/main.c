ea5d29d4bfb9b7f5c8c3cb7340307e3d652a3e2f
Nathan Barrett <nathan.barrett@ndc.com>
2018-01-26 13:52:05 -0500

    Added explanation of complete induction

--- src/main.c
+++ src/main.c
@@ -38,10 +38,21 @@ int main(void){
 
             case COMPLETE_INDUCTION:
 
-                printf("Explanation...\r\n");
+                printf("\r\n\tIn theory -- Proof By Complete Induction (Brute Force) \r\n\r\n");
+                printf("\t    With the exception of number system's overflows (>3999), underflows (<=0), and invalid user inputs,\r\n");
+                printf("\t    we can prove our roman numeral calculator works in all cases by proving that it can convert from:\r\n");
+                printf("\t       - Any decimal value (1-3999) to the correct roman numeral value (I to MMMCMXCIX)\r\n");
+                printf("\t       - Any roman numeral value (I to MMMCMXCIX) to the correct decimal value (1-3999) \r\n");
+                printf("\t    If these two cases can be proven, then we can convert any roman numeral inputs to their decimal equivalents,\r\n");
+                printf("\t    perform basic decimal addition/subtraction on them, and then convert the result back to a roman numeral equivalent\r\n\r\n"); 
+                printf("\t    The file, complete_induction.equations contains 3999 lines (of the form 1=I, 2=II, etc) that provide an input for this proof\r\n\r\n");    
 
                 performTestStory("./complete_induction.equations", EXPRESSION_RN_TO_DEC_COMPARISONS);
 
+                printf("\r\nProof complete!\r\n\r\n");  
+
+                break;
+                
             case MANUAL_ENTRY:
                 return EXIT_SUCCESS;
                 break;
