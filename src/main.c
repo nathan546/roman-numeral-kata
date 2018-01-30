5046fbdc73e46325c2ac9c542b76e8196654fc41
Nathan Barrett Morrison <nathan546@gmail.com>
2018-01-29 20:52:33 -0500

    Added comments to main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
-//TO DO:
-//       README.md
-//       Flow chart
+/*******************************************************************
+* 
+*
+* DESCRIPTION :     Entry point for software and main menu command line parsing functions
+*
+*
+* NOTES :                   
+*
+*              TO DO:
+*                   README.md
+*                   Flow chart?
+*
+*
+* CHANGES :
+*             DATE                  WHO                    DETAIL
+*      August 29, 2018     Nathan Barrett Morrison      Original Code
+*
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,6 +23,10 @@
 
 #include "main.h"
 
+
+//Description:      Main entry point, starts main menu parsing for story selection and manual expression inputs
+//Input Parameters: none
+//Return:           Always true guarantee
 int main(void){
 
     char input;
@@ -27,13 +46,13 @@ int main(void){
             case ADDITION_STORY:
                 printf("%s\r\n", additionStoryDescript);
                 performTestStory("./story_addition.equations", EXPRESSION_RN_OPERATIONS);
-                printf("Testing complete!\r\n");
+                printf("Testing complete!\r\n\r\n");
                 break;
 
             case SUBTRACTION_STORY:
                 printf("%s\r\n", subtractionStoryDescript);
                 performTestStory("./story_subtraction.equations", EXPRESSION_RN_OPERATIONS);
-                printf("Testing complete!\r\n");
+                printf("Testing complete!\r\n\r\n");
                 break;
 
             case COMPLETE_INDUCTION: 
@@ -61,8 +80,13 @@ int main(void){
 
     }
 
+    return 1;
+
 }
 
+//Description:      Performs parsing for manual expression input (addition, subtraction, decimal to roman numeral, roman numeral to decimal, ...)
+//Input Parameters: operation - None
+//Return:           None
 static void manualEntry(){
         char input;
         bool exit = 0;
@@ -105,6 +129,11 @@ static void manualEntry(){
 
 }
 
+
+//Description:      Gets an alphanumeric character from input (stdin)
+//Input Parameters: None
+//Return:           Success:  alphanumeric character that was received from stdin
+//                  Failure:  Blocking unless successful
 static char getAlphanumericChar(){
     char input;
     
@@ -116,10 +145,20 @@ static char getAlphanumericChar(){
     return input;
 }
 
+
+
+//Description:      Check if an input character is numeric (0-9) 
+//Input Parameters: Pointer to input character
+//Return:           Success:  true -- character is numeric (0-9)
+//                  Failure:  false -- character is not numeric
 static bool isNumericChar(char * input){
     return ( (*input >= '0') && (*input <= '9'));
 }
 
+
+//Description:      Waits for the next CR/LF from input (stdin) and flushes all input until received
+//Input Parameters: None
+//Return:           None
 static void flushInput(){
     char input;
     
@@ -129,7 +168,11 @@ static void flushInput(){
 
 }
 
-
+//Description:      Receives input characters into a string until running out of buffer space, hitting CR/LF, or failing a passed in check function
+//Input Parameters: input - string pointer to store input characters into
+//                  functionPointer - pointer to a boolean function which determines if the received character is appropriate or not
+//Return:           Success: true
+//                  Failure: false
 static bool getValue(char * input, bool (*functionPointer)(char*)){
     short i = 0; //Currently parsed string position
     bool doneParsing = 0;
@@ -164,13 +207,16 @@ static bool getValue(char * input, bool (*functionPointer)(char*)){
 }
 
 
+//Description:      Performs a manual addition/subtraction operation (For example, II+I=III or II-I=I) by receiving expression from input (stdin)
+//Input Parameters: romanNumeralOperation - pointer to roman numeral operation structure to be used for storing operands, operators, and results
+//Return:           None
 static void doManualOperation(ROMAN_NUMERAL_OPERATION * romanNumeralOperation){
     printf("Input Operand 1: ");
     if(getValue(romanNumeralOperation->operand1, rnc_is_roman_character)){ //Get a roman numeral value
         printf("Input Operand 2: ");
         if(getValue(romanNumeralOperation->operand2, rnc_is_roman_character)){ //Get a roman numeral value
                 
-                if(rnc_perform_operation(romanNumeralOperation)){
+                if(rnc_perform_operation(romanNumeralOperation)){ //Perform addition/subtraction operation
                     printf("\r\nResult: %s %c %s = %s\r\n\r\n", romanNumeralOperation->operand1,
                                                                 romanNumeralOperation->operator,
                                                                 romanNumeralOperation->operand2,
@@ -187,16 +233,18 @@ static void doManualOperation(ROMAN_NUMERAL_OPERATION * romanNumeralOperation){
     }
 }
 
-
+//Description:      Performs a decimal to roman numeral conversion by receiving decimal value from input (stdin)
+//Input Parameters: None
+//Return:           None
 static void doDecimalToRomanNumeral(){
     char input[MAX_ROMAN_NUMERAL_CHARACTERS];
     int decimalValue;
 
     printf("Input Decimal Integer: ");
 
-    if(getValue(input, isNumericChar)){ //get decimal value as a string
+    if(getValue(input, isNumericChar)){ //Get decimal value as a string
         decimalValue = atoi(input); //Convert string to int
-        if(decimal_to_roman_numeral(decimalValue, input)){
+        if(decimal_to_roman_numeral(decimalValue, input)){ //Convert decimal to roman numeral
             printf("\r\nResult: %d is %s in roman numerals\r\n\r\n", decimalValue, input);
         }else{
             printf("Error: unable to convert decimal value to roman numeral value\r\n");
@@ -206,13 +254,16 @@ static void doDecimalToRomanNumeral(){
     }
 }
 
+//Description:      Performs a roman numeral to decimal conversion by receiving roman numeral value from input (stdin)
+//Input Parameters: None
+//Return:           None
 static void doRomanNumeralToDecimal(){
     char input[MAX_ROMAN_NUMERAL_CHARACTERS];
     int decimalValue;
     printf("Input Roman Numeral: ");
-    if(getValue(input, rnc_is_roman_character)){ //get a roman numeral value
+    if(getValue(input, rnc_is_roman_character)){ //Get a roman numeral value
 
-        if(decimalValue = roman_numeral_to_decimal(input)){
+        if(decimalValue = roman_numeral_to_decimal(input)){ //Convert roman numeral value to decimal
             printf("\r\nResult: %s is %d in decimal\r\n\r\n", input, decimalValue);
         }else{
             printf("Error: Invalid input for roman numeral value\r\n");
